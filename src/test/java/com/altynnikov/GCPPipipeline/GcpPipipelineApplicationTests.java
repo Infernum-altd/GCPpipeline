9e465b4871cdb67376cf3f62a38978e33d43b76b
Infernum-altd <altd0606@gmail.com>
2020-11-21 13:50:47 +0200

    First commmit

---
+++ src/test/java/com/altynnikov/GCPPipipeline/GcpPipipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altynnikov.GCPPipipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpPipipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
