28d012d7f29b264d9e69837504655b435aa6e009
Infernum-altd <altd0606@gmail.com>
2020-11-30 20:50:34 +0200

    PubSubControllerTest done

--- src/test/java/com/altynnikov/GCPPipipeline/PubSubControllerTest.java
+++ src/test/java/com/altynnikov/GCPPipipeline/PubSubControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import java.util.Base64;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PubSubControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -24,8 +26,7 @@ public class PubSubControllerTest {
             "  \"kind\": \"storage#object\",\n" +
             "  \"id\": \"avro-files-storage/severalClients.avsc/1606733502215887\",\n" +
             "  \"selfLink\": \"https://www.googleapis.com/storage/v1/b/avro-files-storage/o/severalClients.avsc\",\n" +
-            "  \"name\":\"testfile.avsc\"";
-
+            "  \"name\":\"testfile.avsc\"}";
 
     @Test
     public void receiveMessageTestOK() throws Exception {
@@ -42,7 +43,7 @@ public class PubSubControllerTest {
     }
 
     @Test
-    public void receiveMessageTest() throws Exception {
+    public void receiveMessageTestServerErrMessageNull() throws Exception {
 
         Body body = new Body();
         ObjectMapper mapper = new ObjectMapper();
@@ -50,7 +51,19 @@ public class PubSubControllerTest {
         mockMvc.perform(post("/receivemsg")
         .contentType("application/json")
         .content(mapper.writeValueAsString(body)))
-                .andExpect(status().is5xxServerError());
+                .andExpect(status().is4xxClientError());
+    }
 
+    @Test
+    public void receiveMessageTestServerErrMessageDataNull() throws Exception {
+
+        Body body = new Body();
+        body.setMessage(new Body.Message("1", "11:00", ""));
+        ObjectMapper mapper = new ObjectMapper();
+
+        mockMvc.perform(post("/receivemsg")
+                .contentType("application/json")
+                .content(mapper.writeValueAsString(body)))
+                .andExpect(status().is4xxClientError());
     }
 }
